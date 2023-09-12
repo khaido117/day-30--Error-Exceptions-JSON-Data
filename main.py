f6d18513bb3c470a4de30cc56067f15230e9009d
Khai Do <khaitroyy@gmail.com>
2023-09-12 17:01:03 -0500

    add popular error examples

---
+++ main.py
@@ -0,0 +1,17 @@
+#------POPULAR ERRORS---------
+
+# #1 File not found
+# with open("file.txt") as file:
+#     file.read()
+
+# #Key Error
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non-existent_key"]
+
+# #IndexError
+# fruit = ["Apple", "Banana"]
+# fruit_item = fruit[3]
+
+# #TypeError
+# text = "abc"
+# print(text + 5)
